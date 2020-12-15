9db4d89fb24d4e1a2c2f069dc32f9d74816ccad7
marc-oclock <marc@oclock.io>
2020-12-15 17:34:54 +0100

    feat: cleaner sliders

---
+++ Slider.js
@@ -0,0 +1,31 @@
+class Slider {
+    static _counter = 0;  
+    x = 200;
+
+    static get counter() {
+        return this._counter++;
+    }
+
+    constructor(name, min, max, current) {
+        this.name = name;
+        
+        this.slider = createSlider(min, max, current);
+        this.y = 10 + 20 * Slider.counter;
+        this.slider.position(this.x, this.y);
+        this.slider.style("width", "300px");
+    }
+
+    draw() {
+        textSize(15);
+        const value = this.value();
+        text(`${this.name} = ${value}`, this.x - 100, this.y + 5);
+    }
+
+    value() {
+        return this.slider.value();
+    }
+
+    input(cb) {
+        this.slider.input(cb);
+    }
+}
--- sketch.js
+++ sketch.js
@@ -12,87 +12,74 @@ const lineThickness = 1;
 
 const center = {};
 
-class Slider {
-  static _counter = 0;  
+function setup() {
+    createSliders();
+    createCanvas(windowWidth, windowHeight);
+    frameRate(5);
 
-  static getCounter() {
-    return this._counter++;
-  }
+    center.x = windowWidth / 2;
+    center.y = windowHeight / 2;
 
-  constructor(min, max, current) {
-    this.slider = createSlider(min, max, current);
-    this.slider.position(10, 10 + 20 * Slider.getCounter());
-    this.slider.style("width", "300px");
-  }
-
-  value() {
-    return this.slider.value();
-  }
-
-  input(cb) {
-    this.slider.input(cb);
-  }
+    init();
 }
 
-function setup() {
-  moduloSlider = new Slider(2, 200, 5);
-  multipSlider = new Slider(2, 200, 20);
-
-  createCanvas(windowWidth, windowHeight);
-  // Put setup code here
-  frameRate(5);
-
-  center.x = windowWidth / 2;
-  center.y = windowHeight / 2;
-
-  moduloSlider.input(init);
-  multipSlider.input(init);
-
-  init();
+function createSliders() {
+    moduloSlider = new Slider("modulo", 2, 200, 5);
+    moduloSlider.input(init);
+    
+    multipSlider = new Slider("table", 2, 200, 20);
+    multipSlider.input(init);
 }
 
 function init() {
-  background(255);
-  fill(0);
-  strokeWeight(lineThickness);
+    background(255);
 
-  positionList = [];
+    positionList = [];
+    
+    moduloSlider.draw();
+    multipSlider.draw();
 
-  modulo = moduloSlider.value();
-  const pas = TWO_PI / modulo;
-  for (let i = 0; i < modulo; i += 1) {
-    const currentPos = [
-      Math.floor(center.x + cos(pas * i) * circleSize),
-      Math.floor(center.y + sin(pas * i) * circleSize),
-    ];
-    positionList.push(currentPos);
-    circle(...currentPos, dotSize);
-  }
 
-  console.log({positionList});
-  loop();
+    fill(0);
+    strokeWeight(lineThickness);
+    
+    modulo = moduloSlider.value();
+
+    const pas = TWO_PI / modulo;
+    let currentPos = [];
+    for (let i = 0; i < modulo; i += 1) {
+        currentPos = [
+            Math.floor(center.x + cos(pas * i) * circleSize),
+            Math.floor(center.y + sin(pas * i) * circleSize),
+        ];
+        positionList.push(currentPos);
+        circle(...currentPos, dotSize);
+    }
+
+    console.log({ positionList });
+    loop();
 }
 
 function draw() {
 
-  fill(15);
-  stroke(0);
+    fill(15);
+    stroke(0);
 
-  multip = multipSlider.value();
-  modulo = moduloSlider.value();
-  for (let i = 0; i < modulo; i += 1) {
-    const from = i;
-    const to = (multip * i) % modulo;
+    multip = multipSlider.value();
+    modulo = moduloSlider.value();
+    for (let i = 0; i < modulo; i += 1) {
+        const from = i;
+        const to = (multip * i) % modulo;
 
-    // console.log("from", from, positionList[from]);
-    // console.log("to", to, positionList[to]);
-    line(positionList[from][0], positionList[from][1], positionList[to][0], positionList[to][1]);
-  }
+        // console.log("from", from, positionList[from]);
+        // console.log("to", to, positionList[to]);
+        line(positionList[from][0], positionList[from][1], positionList[to][0], positionList[to][1]);
+    }
 
-  noLoop();
+    noLoop();
 }
 
 // This Redraws the Canvas when resized
 windowResized = function () {
-  resizeCanvas(windowWidth, windowHeight);
+    resizeCanvas(windowWidth, windowHeight);
 };
